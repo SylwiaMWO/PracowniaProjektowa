1ff86dc6d3652783dc09bddac76fa143881c804d
ikazmier <ikazmier@d17-428-pc-08.ipa.ii.agh.edu.pl>
2023-05-28 11:38:54 +0200

    Raport 3

--- src/main/java/pl/edu/agh/mwo/raports/Raport.java
+++ src/main/java/pl/edu/agh/mwo/raports/Raport.java
@@ -1,4 +1,8 @@
 package pl.edu.agh.mwo.raports;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Raport {
+    public Map<String, Double> analyze(List<List<List<List<Object>>>> workbookDataList);
 }
--- src/main/java/pl/edu/agh/mwo/raports/RaportOne.java
+++ src/main/java/pl/edu/agh/mwo/raports/RaportOne.java
@@ -2,32 +2,24 @@ package pl.edu.agh.mwo.raports;
 
 import java.util.*;
 
-public class RaportOne {
+public class RaportOne implements Raport {
 
     public Map<String, Double> analyze(List<List<List<List<Object>>>> workbookDataList) {
 
         Map<String, Double> sheetTimeSums = new HashMap<>();
 
-
         for (List<List<List<Object>>> workbookData : workbookDataList) {
-
-
             for (List<List<Object>> sheetData : workbookData) {
-
-
                 List<Object> firstRow = sheetData.get(0);
                 String sheetName = (String) firstRow.get(0);
 
-
                 double sheetSum = 0;
-
 
                 for (int i = 2; i < sheetData.size(); i++) {
                     List<Object> row = sheetData.get(i);
 
 
                     if (row.size() >= 3) {
-                        // Get the "Czas [h]" cell, assume it's a Double
                         Double cellValue = (Double) row.get(2);
                         sheetSum += cellValue;
                     }
