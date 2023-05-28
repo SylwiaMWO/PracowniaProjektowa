c3682ebf9a44902666c7f1e489d0a8653134c1af
jgrela <jgrela@d17-428-pc-10.ipa.ii.agh.edu.pl>
2023-05-28 09:15:38 +0200

    Add writing report to console and excel

--- src/main/java/pl/edu/agh/mwo/converters/data_to_excel.java
+++ src/main/java/pl/edu/agh/mwo/converters/data_to_excel.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.mwo.converters;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,14 +12,14 @@ import  org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import  org.apache.poi.hssf.usermodel.HSSFRow;
 
 public class data_to_excel {
-    HashMap<String, String[]> report_one = new HashMap<>();
-    HashMap<String, String[]> report_two = new HashMap<>();
-    HashMap<String, String[]> report_three = new HashMap<>();
 
-    public void initializeHashMaps(){
-        report_one.put("Projekty:", new String[]{});
-        report_one.put("Czas:", new String[]{});
-    }
+//    HashMap<String, String[]> report_two = new HashMap<>();
+//    HashMap<String, String[]> report_three = new HashMap<>();
+
+//    public void initializeHashMaps(){
+//        report_one.put("Projekty:", new String[]{});
+//        report_one.put("Czas:", new String[]{});
+//    }
 
 
     /*Printowanie hashmapy w formacie:
@@ -56,11 +58,11 @@ public class data_to_excel {
     }
 
     /*Kurde nie wiem czy zadziała, będziemy testować na produkcji :P */
-    public void readHashMapToExcel(HashMap<String, String[]> mapToRead){
+    public static void readHashMapToExcel(HashMap<String, String[]> mapToRead) throws IOException {
         int rowWidth = mapToRead.size();
         int i = 0;
         int arrayLength = getArrayLength(mapToRead);
-        String filename = "C:/Raport.xls" ;
+        String filename = "/home/students/j/g/jgrela/pracownia/PracowniaProjektowa/DataRaport.xls" ;
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("Raport");
         HSSFRow rowHead = sheet.createRow((short)0);
@@ -68,14 +70,25 @@ public class data_to_excel {
             rowHead.createCell(i).setCellValue(key);
             i+=1;
         }
-        for(int step = 0; i < arrayLength; i++){
+        for(int step = 0; step < arrayLength; step++){
             int j = 0;
             HSSFRow row = sheet.createRow((short)step+1);
             for(String key : mapToRead.keySet()){
-                row.createCell(j).setCellValue(mapToRead.get(key)[step+1]);
+                row.createCell(j).setCellValue(mapToRead.get(key)[step]);
                 j+=1;
             }
         }
+        FileOutputStream fileOut = new FileOutputStream(filename);
+        workbook.write(fileOut);
+    }
+
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String[]> report_one = new HashMap<>();
+        report_one.put("Test1", new String[]{"2", "3", "5"});
+        report_one.put("Test2", new String[]{"6", "7", "8"});
+        report_one.put("Test3", new String[]{"9", "10", "11"});
+        readHashMapToExcel(report_one);
+        printHashMap(report_one);
     }
 
 }
